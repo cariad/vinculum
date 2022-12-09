633b31fce027855982f6f545c8485ed66d093e94
Cariad Eccleston <cariad@cariad.earth>
2022-12-09 10:00:20 +0000

    Add __eq__ and reduced

--- tests/test_fraction.py
+++ tests/test_fraction.py
@@ -3,6 +3,10 @@ from pytest import mark
 from vinculum import Fraction
 
 
+def test_eq__unhandled_type() -> None:
+    assert Fraction(0) != "zero"
+
+
 @mark.parametrize(
     "n, d, expect_n, expect_d",
     [
@@ -17,3 +21,16 @@ def test_init(n: int, d: int, expect_n: int, expect_d: int) -> None:
     f = Fraction(n, d)
     assert f.denominator == expect_d
     assert f.numerator == expect_n
+
+
+@mark.parametrize(
+    "f, expect",
+    [
+        (Fraction(0), Fraction(0)),
+        (Fraction(1), Fraction(1)),
+        (Fraction(1, 2), Fraction(1, 2)),
+        (Fraction(15, 30), Fraction(1, 2)),
+    ],
+)
+def test_reduced(f: Fraction, expect: Fraction) -> None:
+    assert f.reduced == expect

bfd2aebe3eb243438d0b4d0dc173df365ea69deb
Cariad Eccleston <cariad@cariad.earth>
2022-12-09 09:18:07 +0000

    Add numerator and denominator (#2)

---
+++ tests/test_fraction.py
@@ -0,0 +1,19 @@
+from pytest import mark
+
+from vinculum import Fraction
+
+
+@mark.parametrize(
+    "n, d, expect_n, expect_d",
+    [
+        (0, 0, 0, 0),
+        (1, 2, 1, 2),
+        (-1, 2, -1, 2),
+        (1, -2, -1, 2),
+        (-1, -2, 1, 2),
+    ],
+)
+def test_init(n: int, d: int, expect_n: int, expect_d: int) -> None:
+    f = Fraction(n, d)
+    assert f.denominator == expect_d
+    assert f.numerator == expect_n
